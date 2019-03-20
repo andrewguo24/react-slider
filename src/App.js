a568c753147c39c0b8528cf63bac2f3f5962318b
Ge Guo <alex@Ges-MacBook-Pro.local>
2019-03-20 12:31:02 +1100

    updated

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./App.scss";
 
-class App extends Component {
+export default class App extends React.Component {
+  state = {
+    isSliderOpen: false,
+    isAnimationPerformed: false
+  };
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      isSliderOpen: !prevState.isSliderOpen,
+      isAnimationPerformed: true
+    }));
+  };
+
   render() {
+    const { isSliderOpen, isAnimationPerformed } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <button className="button" onClick={() => this.handleClick()}>
+          {isSliderOpen ? "Close" : "Open"}
+        </button>
+        <div
+          className={`container ${isSliderOpen ? "open" : "closed"} ${
+            isAnimationPerformed ? "is-animation-performed" : ""
+          } `}
+        >
+          <div className="slider">This is a slider</div>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  isSliderOpen: PropTypes.bool,
+  isAnimationPerformed: PropTypes.bool
+};
